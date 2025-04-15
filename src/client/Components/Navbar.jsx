59f09da8cb65ac668b6307575a03069f444d01fe
Dan Sealy <dan.sealy5@gmail.com>
2025-04-15 13:26:08 -0400

    listing navbar links

--- src/client/Components/Navbar.jsx
+++ src/client/Components/Navbar.jsx
@@ -1,32 +1,37 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const { token, setToken } = useAuth();
+
   return (
     <nav className="navbar">
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        
-          <li>
-            <Link to="/account">Account</Link>
-          </li>
-        
-        
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        
-          <li>
-            <Link to="/" onClick={() => handleLoginLogout(false)}>
-              Logout
-            </Link>
-          </li>
-        
+
+        <li>
+          <Link to="/explore">Explore</Link>
+        </li>
+
+        <li>
+          <Link to="/get-found">Get Found</Link>
+        </li>
+
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+
+        <li>
+          <Link to="/" onClick={() => setToken(false)}>
+            Logout
+          </Link>
+        </li>
       </ul>
     </nav>
   );
