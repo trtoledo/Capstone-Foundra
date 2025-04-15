39527b2c33beae52bb324cc25062f5789a620fd9
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-04-15 12:53:13 -0400

    register.jsx done for now

--- src/client/Components/Auth/Login.jsx
+++ src/client/Components/Auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login = ({ setToken }) => {
+const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [successMessage, setSuccessMessage] = useState(null);
