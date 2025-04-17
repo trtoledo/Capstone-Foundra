ea81bd665ad3653c75dfafd6a2f45578b4a1049c
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-04-17 18:41:52 -0400

    one dashboard with conditional rendering and videos.jsx added

--- src/client/Components/MangerDashboard.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const ManagerDashboard = () => {
-    return ( 
-        <h1>ManagerDashboard</h1>
-     );
-}
- 
-export default ManagerDashboard;
---
+++ src/client/Components/Videos.jsx
@@ -0,0 +1,9 @@
+
+
+const Videos = () => {
+    return ( 
+        <h2>Videos</h2>
+     );
+};
+ 
+export default Videos;
