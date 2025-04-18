197b7fb0fc27bfa0c504df9377c55143a6b4b278
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-04-18 12:18:27 -0400

    added routes and imported components

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Explore from './Components/Explore.jsx';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar.jsx';
+import Candidates from './Components/Candidates.jsx';
+import Companies from './Components/Companies.jsx';
+import Dashboard from './Components/Dashboard.jsx';
+import SingleIndustryPage from './Components/SingleIndustryPage.jsx';
+import TopCandidates from './Components/TopCandidates.jsx';
+import Videos from './Components/Videos.jsx';
 
 const App = ()=> {
   return (
@@ -11,6 +17,12 @@ const App = ()=> {
       <Routes>
         <Route path='/' element={ <Home /> } />
         <Route path='/explore' element={ <Explore /> } />
+        <Route path='/candidates' element={ <Candidates />} />
+        <Route path='/companies' element={ <Companies /> } />
+        <Route path='/dashboard' element={ <Dashboard /> } />
+        <Route path='/industry/:id' element={ <SingleIndustryPage /> } />
+        <Route path='/candidates/top' element={ <TopCandidates /> } />
+        <Route path='/videos' element={ <Videos /> } />
       </Routes>
     </div>
   );
