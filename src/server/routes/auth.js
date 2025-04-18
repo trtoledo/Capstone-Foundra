466cd1a704482c472f8536886b60159216ec55ee
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-04-18 16:35:09 -0400

    fixed passing of role

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -49,7 +49,7 @@ router.post("/login", async (req, res) => {
       { expiresIn: "1h" }
     );
 
-    res.json({ token, Role });
+    res.json({ token, role: user.role, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
