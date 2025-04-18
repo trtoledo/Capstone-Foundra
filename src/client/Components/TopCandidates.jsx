405477929c195051c665784bc9f5cb1f8aa32018
Dan Sealy <dan.sealy5@gmail.com>
2025-04-18 14:17:48 -0400

    laying base work for topCandidates

--- src/client/Components/TopCandidates.jsx
+++ src/client/Components/TopCandidates.jsx
@@ -1,9 +1,50 @@
-
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {fetchTopCandidates, createTopCandidate, deleteTopCandidate,} from "../api/topCandidates";
 
 const TopCandidates = () => {
-    return ( 
-        <h4>Top Candidates</h4>
-     );
-}
- 
+  const [topCandidates, setTopCandidates] = useState([]);
+  const [searchParam, setSearchParam] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function renderTopCandidates() {
+      const topCandidates = await fetchTopCandidates();
+      setTopCandidates(topCandidates);
+    }
+    renderTopCandidates();
+  }, []);
+
+  const topCandidatesToDisplay = searchParam
+    ? topCandidates.filter((idx) =>
+        idx.name.toLowerCase().includes(searchParam)
+      )
+    : topCandidates;
+
+  return (
+    <>
+      <div className="search">
+        <label>
+          Search:{" "}
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
+          />
+        </label>
+      </div>
+      <div className="allTopCandidates">
+        {(topCandidates || []).map((idx) => {
+          return (
+            <div key={idx.id} className="topCandidate">
+              <h4>{idx.name}</h4>
+              <h6>{idx.email}</h6>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
 export default TopCandidates;
