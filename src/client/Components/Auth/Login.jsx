edf95fc1210de17d4351582e9af2e28f03384895
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-04-18 17:37:31 -0400

    proper import of loginUser in Login.jsx

--- src/client/Components/Auth/Login.jsx
+++ src/client/Components/Auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
-import { loginUser } from "../api/auth";
+import { loginUser } from '../../api/auth.js';
 
 const Login = () => {
   const { setUser, setToken, setRole, setRefresh } = useAuth();
