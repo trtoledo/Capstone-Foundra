054133f2a57be8b38ebb534dec01389da290384d
Dan Sealy <dan.sealy5@gmail.com>
2025-04-21 13:47:05 -0400

    navbar change for conditional rendering

--- src/client/Components/Layout/Navbar.jsx
+++ src/client/Components/Layout/Navbar.jsx
@@ -8,6 +8,11 @@ import { useAuth } from "../Context/AuthContext";
 const Navbar = () => {
   const { token, setToken } = useAuth();
 
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
   return (
     <nav className="navbar">
       {/* <div className="logo">
@@ -16,31 +21,21 @@ const Navbar = () => {
         </a>
       </div> */}
       <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-
-        <li>
-          <a href="/explore">Explore</a>
-        </li>
-
-        <li>
-          <a href="/get-found">Get Found</a>
-        </li>
-
-        <li>
-          <a href="/register">Register</a>
-        </li>
-
-        <li>
-          <a href="/login">Login</a>
-        </li>
-
-        <li>
-          <a href="/" onClick={() => setToken(false)}>
-            Logout
-          </a>
-        </li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/explore">Explore</a></li>
+        <li><a href="/get-found">Get Found</a></li>
+        {!token && (
+          <>
+            <li><a href="/register">Register</a></li>
+            <li><a href="/login">Login</a></li>
+          </>
+        )}
+        {token && (
+          <>
+          <li><a href="/" onClick={handleLogout}>Logout</a></li>
+          {/* <li><span>Welcome, {role}</span></li> */}
+          </>
+        )}
       </ul>
     </nav>
   );
